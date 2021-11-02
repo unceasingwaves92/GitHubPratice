df5659b51b849c2011e8bb0edecc05cc60fb0449
unceasingwaves92 <unceasingwaves92@gmail.com>
2021-11-02 19:56:36 +0530

    Added/Updated/deleted

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -5,5 +5,8 @@ public class LoginPage {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello LP");
+		String un = "admin";
+		String pwd = "admin";
+		System.out.println("logged in...");
 	}
 }
---
+++ src/Pages/SearchPage.java
@@ -0,0 +1,10 @@
+package Pages;
+
+public class SearchPage {
+	
+public static void main(String[] args) {
+		
+		System.out.println("Hello SP");
+	}
+
+}

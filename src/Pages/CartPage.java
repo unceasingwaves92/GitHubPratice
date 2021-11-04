0365c066b9349f6241d1eda1e8dec5db713e51ba
unceasingwaves92 <unceasingwaves92@gmail.com>
2021-11-04 18:45:13 +0530

    updated cart page

--- src/Pages/CartPage.java
+++ src/Pages/CartPage.java
@@ -3,7 +3,10 @@ package Pages;
 public class CartPage {
 	
 	public static void main(String[] args) {
+		
 		System.out.println("cart page");
+		
+		System.out.println("cart page feature");
 	}
 
 }

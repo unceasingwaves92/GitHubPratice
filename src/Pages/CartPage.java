800495779e498da9b72f6dce817d186a36032662
unceasingwaves92 <unceasingwaves92@gmail.com>
2021-11-05 19:34:54 +0530

    Added cartPage features

---
+++ src/Pages/CartPage.java
@@ -0,0 +1,10 @@
+package Pages;
+
+public class CartPage {
+	
+		public static void doCart() {
+		
+		System.out.println("Cart Page");
+	}
+
+}
---
+++ src/tests/CartPageTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class CartPageTest {
+	
+		
+	public void cartTest() {
+		
+		System.out.println("true");
+	}
+
+
+}

ea5bba03aea84dff2b9e468f16a7fd250c2650db
unceasingwaves92 <unceasingwaves92@gmail.com>
2021-11-05 19:44:00 +0530

    updated cart page code

--- src/Pages/CartPage.java
+++ src/Pages/CartPage.java
@@ -5,6 +5,8 @@ public class CartPage {
 		public static void doCart() {
 		
 		System.out.println("Cart Page");
+		String orderId = "123";
+		System.out.println("orderId");
 	}
 
 }
--- src/tests/CartPageTest.java
+++ src/tests/CartPageTest.java
@@ -1,11 +1,15 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class CartPageTest {
 	
-		
+	@Test
 	public void cartTest() {
 		
 		System.out.println("true");
+		Assert.assertEquals(true, true);
 	}
 
 
